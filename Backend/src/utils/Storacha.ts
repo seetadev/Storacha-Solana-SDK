79a0f61abe880fc8a699d9779bd5975ba6ce4482
Devansh Goel <117383704+Devanshgoel-123@users.noreply.github.com>
2025-08-17 23:43:59 +0530

    Added traxn Instruction in the upload route (#15)
    Co-authored-by: Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>

--- Backend/src/utils/Storacha.ts
+++ Backend/src/utils/Storacha.ts
@@ -5,6 +5,8 @@ import * as Proof from "@storacha/client/proof";
 import { Signer } from "@storacha/client/principal/ed25519";
 import { QuoteInput } from "../types/StorachaTypes.js";
 import { ADMIN_CONFIG } from "../config/config.js";
+import { db } from "../db/db.js";
+import { configTable } from "../db/schema.js";
 
 /**
  * Initializes a Storacha client using user-provided key and proof.
@@ -27,13 +29,17 @@ export async function initStorachaClient(): Promise<Client.Client> {
 /**
  * This function returns the amount it will cost to upload the file, keep it for minimum duration
  */
-export const getQuoteForFileUpload = ({
+export const getQuoteForFileUpload = async ({
   durationInUnits,
   sizeInBytes,
 }: QuoteInput) => {
-  const MINIMUM_DURATION_UNIT = ADMIN_CONFIG.minimumDuration;
-  const RATE_PER_BYTE_PER_UNIT = ADMIN_CONFIG.rate; // e.g. in SOL; or use lamports: 1000 lamports/byte/day
-
+  const data = await db
+    .select({
+      MINIMUM_DURATION_UNIT: configTable.min_duration_days,
+      RATE_PER_BYTE_PER_UNIT: configTable.rate_per_byte_per_day,
+    })
+    .from(configTable); // e.g. in SOL; or use lamports: 1000 lamports/byte/day
+  const { MINIMUM_DURATION_UNIT, RATE_PER_BYTE_PER_UNIT } = data[0];
   const effectiveDuration = Math.max(durationInUnits, MINIMUM_DURATION_UNIT);
   const totalCost = sizeInBytes * effectiveDuration * RATE_PER_BYTE_PER_UNIT;
 
@@ -45,7 +51,7 @@ export const getQuoteForFileUpload = ({
 };
 
 /**
- * This function is used to deode a delegation which is provided as a proof by a certain agent to access a space
+ * This function is used to decode a delegation which is provided as a proof by a certain agent to access a space
  */
 
 export const DecodeDelegations = () => {};
