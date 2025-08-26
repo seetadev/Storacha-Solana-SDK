24598b45bab6c451657b43f7de2ace1b33a8f90b
Harsh Singh <harshsingh16d@gmail.com>
2025-08-27 00:50:59 +0530

    fix: updated lock file

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -49,7 +49,7 @@ export class UploadService {
 
   constructor() {
     this.connection = new Connection(clusterApiUrl("testnet"), "confirmed");
-    this.apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5040/api/user";
+    this.apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5040/api";
   }
 
   async uploadFileWithDeposit(
