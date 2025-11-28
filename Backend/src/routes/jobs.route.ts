c0366428a54303c1f10b8e89b2c4b639eadac5d8
kaf-lamed-beyt <belac335@gmail.com>
2025-11-28 13:38:09 +0100

    feat: run deletion job for expired file uploads

--- Backend/src/routes/jobs.route.ts
+++ Backend/src/routes/jobs.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import * as cronController from "../controllers/jobs.controller.js";
+import * as jobsController from "../controllers/jobs.controller.js";
 import { verifyQStashRequest } from "../middleware/qstash.middleware.js";
 
 export const jobs = express.Router();
@@ -7,11 +7,11 @@ export const jobs = express.Router();
 jobs.post(
   "/send-warnings",
   verifyQStashRequest,
-  cronController.sendExpirationWarnings,
+  jobsController.sendExpirationWarnings,
 );
 
 jobs.post(
   "/delete-expired",
   verifyQStashRequest,
-  cronController.deleteExpiredDeposits,
+  jobsController.deleteExpiredDeposits,
 );
