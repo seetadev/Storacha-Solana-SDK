708182e94a1e1255d516336b06f3ef0f15e14748
kaf-lamed-beyt <belac335@gmail.com>
2026-01-08 15:05:19 +0100

    feat: add environment setup ahead of mainnet launch
    i've gone ahead and created the neccessary platform/services account we need and deployed new instances of the server on render for both staging and production.
    
    including a staging and production db on neon as well. right now, we'll wait until we're set for mainnet and buy SOL so i can generate the neccessary keys for mainnet deployment
    
    i've also removed a couple of redundant files/scripts that we may not be needing anymore.

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -1,7 +1,19 @@
-export const ENDPOINT =
-  typeof window !== 'undefined' && window.location.hostname === 'localhost'
-    ? 'http://localhost:5040'
-    : 'https://storacha-solana-sdk-bshc.onrender.com';
+const ENDPOINTS = {
+  local: 'http://localhost:5040',
+  staging: 'https://storacha-solana-sdk-o9t9.onrender.com',
+  production: 'https://keep-sdk-prod.onrender.com',
+} as const;
+
+function getEndpoint(): string {
+  if (typeof window !== 'undefined' && window.location.hostname === 'localhost')
+    return ENDPOINTS.local;
+  if (typeof window === 'undefined' && process.env.NODE_ENV === 'development')
+    return ENDPOINTS.local;
+
+  return ENDPOINTS.production;
+}
+
+export const ENDPOINT = getEndpoint();
 
 export const DAY_TIME_IN_SECONDS = 86400;
 
--- server/src/routes/jobs.route.ts
+++ server/src/routes/jobs.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import * as jobsController from "../controllers/jobs.controller.js";
-import { verifyQStashRequest } from "../middleware/qstash.middleware.js";
+import { verifyQStashRequest } from "../middlewares/qstash.middleware.js";
 
 export const jobs = express.Router();
 
