bbf2cb5dc45c1192ac41423f95093dfd5f112a1a
kaf-lamed-beyt <belac335@gmail.com>
2026-02-03 11:07:14 +0100

    chore: add terms of use

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export function isMasterChief(req: Request, res: Response, next: NextFunction) {
   const apiKey = req.headers["x-api-key"];
   if (!apiKey || apiKey !== process.env.ADMIN_API_KEY) {
-    return res.status(403).json({ error: "Who are you!" });
+    return res.status(403).json({ error: "Who goes you?!" });
   }
   next();
 }
